1c03a5ee7e4482a54524e86f10e2c2cbdc6d95e1
prakashbharti786 <prakashbharti786@gmail.com>
2017-02-12 22:57:30 +0530

    card style css in developemnt

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,6 @@ let mix = require('laravel-mix').mix;
 
 let env = 'test/';
 
-mix.sass('resources/assets/sass/codeartisan-ui.scss',env+'css/codeartisan.css');
+//mix.sass('resources/assets/sass/codeartisan-ui.scss',env+'css/codeartisan.css');
+mix.sass('resources/assets/sass/current.scss',env+'css/current.css');
 //mix.js('resources/assets/js/button.js',env+'js/button.js')
