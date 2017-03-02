4ce17decddb725ca7789e1d8a35e658b4cd441a4
prakashbharti786 <prakashbharti786@gmail.com>
2017-03-02 17:54:52 +0530

    md chips component added

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,6 +2,7 @@ let mix = require('laravel-mix').mix;
 
 let env = 'test/';
 
-mix.sass('resources/assets/sass/codeartisan-ui.scss',env+'css/codeartisan.css');
+//mix.sass('resources/assets/sass/codeartisan-ui.scss',env+'css/codeartisan.css');
 //mix.sass('resources/assets/sass/current.scss',env+'css/current.css');
+mix.sass('resources/assets/sass/current.scss',env+'css/current.css');
 //mix.js('resources/assets/js/button.js',env+'js/button.js')
